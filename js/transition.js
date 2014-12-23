a293024e94cfe26ca5f939b15655141e1b36b41f
schurc01 <sjchurch18@gmail.com>
2014-12-23 13:43:49 -0500

    fixed fonts

--- js/transition.js
+++ js/transition.js
@@ -1,18 +1,3 @@
-// $(document).ready(function() {
-//         		$("body").css("display", "none");
-//         		$("body").fadeIn(2000);
-
-//     			$("a.transition").click(function(event){
-//         			event.preventDefault();
-//         			linkLocation = this.href;
-//         			$("body").fadeOut(1000, redirectPage);      
-//     			});
-         
-//     			function redirectPage() {
-//        				window.location = linkLocation;
-//     			}
-// });
-
 $(document).ready(function() {
     $(window).scroll(function (event) {
         var y = $(this).scrollTop();
