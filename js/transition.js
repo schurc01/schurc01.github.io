f3e6e4b984187e3c36fd47c69fec5d1ed5c2f348
schurc01 <sjchurch18@gmail.com>
2014-12-23 12:59:45 -0500

    added scrolling

--- js/transition.js
+++ js/transition.js
@@ -1,20 +1,25 @@
-$(document).ready(function() {
-        		$("body").css("display", "none");
-        		$("body").fadeIn(2000);
+// $(document).ready(function() {
+//         		$("body").css("display", "none");
+//         		$("body").fadeIn(2000);
 
-    			$("a.transition").click(function(event){
-        			event.preventDefault();
-        			linkLocation = this.href;
-        			$("body").fadeOut(1000, redirectPage);      
-    			});
+//     			$("a.transition").click(function(event){
+//         			event.preventDefault();
+//         			linkLocation = this.href;
+//         			$("body").fadeOut(1000, redirectPage);      
+//     			});
          
-    			function redirectPage() {
-       				window.location = linkLocation;
-    			}
-                $(".contact").hover(function() {
-                    console.log("hello");
-                     $(this).animate({marginTop: 100});
-                     var email = "sjchurch18@gmail.com";
-                     $(#email).html(email);
-                });
+//     			function redirectPage() {
+//        				window.location = linkLocation;
+//     			}
+// });
+
+$(document).ready(function() {
+    $(window).scroll(function (event) {
+        var y = $(this).scrollTop();
+        if (y > 1) {
+            $('#navbar').addClass('move');
+        } else {
+            $('#navbar').removeClass('move');
+        }
+    });
 });
