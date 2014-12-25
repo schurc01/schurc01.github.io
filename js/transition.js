538f1154839e154e02920c04f3d6d3a592c33fda
schurc01 <sjchurch18@gmail.com>
2014-12-24 21:49:27 -0500

    added next sections

--- js/transition.js
+++ js/transition.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
             $('#navbar').removeClass('move');
         }
     });
+    $('a[href^="#"]').on('click',function (e) {
+    		//understand
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
